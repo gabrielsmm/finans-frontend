5d3db3d564530e1a8085a6e0ccbe2679b6eb8862
gabrielsmm <gabrielsmm251@gmail.com>
2023-06-26 20:55:51 -0300

    refactor: Melhorias na responsividade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
   {path: 'visao-geral', loadChildren: () => import('./pages/visao-geral/visao-geral.module').then(m => m.VisaoGeralModule), canActivate: [AuthGuard]},
   {path: 'orcamentos', loadChildren: () => import('./pages/orcamentos/orcamentos.module').then(m => m.OrcamentosModule), canActivate: [AuthGuard]},
   {path: 'transacoes', loadChildren: () => import('./pages/transacoes/transacoes.module').then(m => m.TransacoesModule), canActivate: [AuthGuard]},
-  {path: '**', loadChildren: () => import('./pages/visao-geral/visao-geral.module').then(m => m.VisaoGeralModule)},
-  {path: '', redirectTo: 'visao-geral', pathMatch: 'full'}
+  {path: '**', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
 @NgModule({
